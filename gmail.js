8120c194e9aaa5739fd118e58f167625435cbba8
Brook Seyoum <88465473+Turmaxx@users.noreply.github.com>
2026-07-03 18:09:55 +0300

    add gmail.js

---
+++ gmail.js
@@ -0,0 +1,36 @@
+let previousTab = null;
+
+function createPinnedTab() {
+  return browser.tabs.create({
+    url: "https://mail.google.com/mail/u/0/",
+    pinned: true,
+    active: true
+  });
+}
+
+async function handleClick(tab) {
+  const currentTabId = tab.id;
+
+  const gmailTabs = await browser.tabs.query({
+    url: "https://mail.google.com/*"
+  });
+
+  if (gmailTabs.length === 0) {
+    previousTab = currentTabId;
+    await createPinnedTab();
+    return;
+  }
+
+  const gmailTabId = gmailTabs[0].id;
+
+  if (gmailTabId === currentTabId) {
+    if (previousTab !== null) {
+      browser.tabs.update(previousTab, { active: true });
+    }
+  } else {
+    previousTab = currentTabId;
+    browser.tabs.update(gmailTabId, { active: true });
+  }
+}
+
+browser.browserAction.onClicked.addListener(handleClick);
